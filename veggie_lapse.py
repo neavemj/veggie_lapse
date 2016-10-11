7c7c16a994dce746ebd50fbb4c3700ef0908e997
neavemj <matthewjneave1@gmail.com>
2016-10-12 10:57:42 +1100

    implemented scheduling with apscheduler

--- veggie_lapse.py
+++ veggie_lapse.py
@@ -2,3 +2,22 @@
 
 # timelapse of veg
 
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+import time
+
+
+def print_time():
+    print time.asctime()
+
+
+# main scheduling calls
+
+sched = BlockingScheduler()
+
+trigger = CronTrigger(second="*")
+
+sched.add_job(print_time, trigger)
+
+sched.start()
