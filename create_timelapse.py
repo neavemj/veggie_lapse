3da17fc27c5c71f951b5093b99259b6709381c01
neavemj <matthewjneave1@gmail.com>
2016-11-10 21:34:14 +1100

    added set operations for photos

--- create_timelapse.py
+++ create_timelapse.py
@@ -9,6 +9,6 @@ import subprocess
 # -f = frame rate; -start_number = first photo number; -i = where integer is in the filename
 # -s = resolution; -vcodec = not sure; last bit is video name
 
-#./ffmpeg -r 10 -y -start_number 4146 -i ./photos/GOPR%*.JPG -vf "crop=h=2250" -c:v libx264 -pix_fmt yuv420p -crf 20 -s 1280x720 veggie_lapse.mp4
+#./ffmpeg -r 25 -y -start_number 4146 -i ./photos/GOPR%*.JPG -vf "crop=h=2250" -c:v libx264 -pix_fmt yuv420p -crf 20 -s 1280x720 veggie_lapse.mp4
 
 subprocess.run(["ffmpeg", "-f", "10", "-start_number", "4146", "-i", "./photos/GOPR%*.JPG", "-s", "1280x720", "-vcodec", "libx264", "veggie_lapse.mp4"], shell=True)
--- get_pics.py
+++ get_pics.py
@@ -18,7 +18,7 @@ def connect_gopro():
     """
     print "connecting to gopro"
     cam = GoProHero(password="matts_gopro")
-    time.sleep(10)
+    time.sleep(5)
     status = cam.status()
     if status["summary"] == "notfound":
         print "could not connect to camera"
@@ -33,11 +33,21 @@ def wake_gopro(cam):
     time.sleep is used to ensure time for the camera to wake
     """
     print "waking gopro"
-    result = cam.command("power", "on")
-    time.sleep(10)
+    result = cam.command("power.JPG", "on")
+    time.sleep(5)
     if result == False:
         print "could not wake gopro, this probably won't work"
     return result
+
+def sleep_gopro(cam):
+    """
+    attempts to sleep the gopro
+    """
+    print "putting gopro to sleep"
+    result = cam.command("power.JPG", "sleep")
+    time.sleep(5)
+    if result == False:
+        print "could not sleep gopro"
 
 # connect and wake gopro
 
@@ -74,6 +84,20 @@ computer_photo_set = set(os.listdir("./photos/"))
 
 new_photos = photo_set - computer_photo_set
 
+unwanted_photos = set(["GOPR4308.JPG","GOPR4310.JPG","GOPR4311.JPG","GOPR4312.JPG","GOPR4313.JPG","GOPR4314.JPG","GOPR4315.JPG",\
+"GOPR4316.JPG","GOPR4317.JPG","GOPR4318.JPG","GOPR4319.JPG","GOPR4320.JPG","GOPR4321.JPG","GOPR4378.JPG","GOPR4379.JPG","GOPR4164.JPG",\
+"GOPR4180.JPG","GOPR4197.JPG","GOPR4214.JPG","GOPR4327.JPG","GOPR4310.JPG","GOPR4339.JPG","GOPR4352.JPG","GOPR4381.JPG","GOPR4383.JPG",\
+"GOPR4397.JPG","GOPR4163.JPG","GOPR4167.JPG","GOPR4226.JPG","GOPR4239.JPG","GOPR4256.JPG","GOPR4269.JPG","GOPR4288.JPG","GOPR4325.JPG",\
+"GOPR4327.JPG","GOPR4339.JPG","GOPR4352.JPG","GOPR4380.JPG","GOPR4165.JPG","GOPR4166.JPG","GOPR4183.JPG","GOPR4198.JPG","GOPR4199.JPG",\
+"GOPR4200.JPG","GOPR4213.JPG","GOPR4215.JPG","GOPR4216.JPG","GOPR4223.JPG","GOPR4225.JPG","GOPR4240.JPG","GOPR4241.JPG","GOPR4258.JPG",\
+"GOPR4259.JPG","GOPR4268.JPG","GOPR4270.JPG","GOPR4271.JPG","GOPR4286.JPG","GOPR4287.JPG","GOPR4303.JPG","GOPR4305.JPG","GOPR4309.JPG",\
+"GOPR4324.JPG","GOPR4325.JPG","GOPR4326.JPG","GOPR4327.JPG","GOPR4340.JPG","GOPR4341.JPG","GOPR4350.JPG","GOPR4351.JPG","GOPR4363.JPG",\
+"GOPR4364.JPG","GOPR4384.JPG","GOPR4396.JPG","GOPR4398.JPG","GOPR4405.JPG","GOPR4406.JPG","GOPR4407.JPG","GOPR4415.JPG","GOPR4416.JPG",\
+"GOPR4417.JPG","GOPR4116.JPG","GOPR4147.JPG","GOPR4148.JPG","GOPR4182.JPG","GOPR4184.JPG","GOPR4185.JPG","GOPR4224.JPG","GOPR4242.JPG",\
+"GOPR4257.JPG","GOPR4304.JPG","GOPR4146.JPG","GOPR4181.JPG","GOPR4309.THM","GOPR4309.MP4","GOPR4309.LRV"])
+
+new_photos = new_photos - unwanted_photos
+
 if len(new_photos) == 0:
     print("there are no new photos to download\nexiting\n")
 
@@ -81,19 +105,19 @@ else:
     print("you have {} new photos to download".format(len(new_photos)))
     ans = raw_input("Would you like to download them now (y/n)? ")
 
-# 4308 is up-side down, 4310, 4311, 4312-4321, 4378, 4379 wrong spot
-
 if ans == "y":
-	photos_remaining = len(new_photos)
+    photos_remaining = len(new_photos)
     for photo in new_photos:
-    	print("photos to download:", str(photos_remaining))
-        print("downloading file:", photo)
+        print("photos to download:.JPG", str(photos_remaining))
+        print("downloading file:.JPG", photo)
         photo_url = go_pro_url + photo 
         wget.download(photo_url, "./photos/")
         photos_remaining -= 1
-    print("\nfinished downloading photos")      
+    print("\nfinished downloading photos")
+    sleep_gopro(cam)      
 else:
     print("ok, exiting..")
+    sleep_gopro(cam)
 
 
 
